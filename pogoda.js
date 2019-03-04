7bfa29b7999afb8374b6d617c4fb4d69648e7846
kulikowskidawid <kulikowskidawid@outlook.com>
2019-03-04 20:57:30 +0100

    Deklaracja Klasy Pogoda z Konstruktorem

---
+++ pogoda.js
@@ -0,0 +1,20 @@
+// Uzyskany Klucz API z Wunderground cc557fbc884831f9
+
+// Deklaracja Klasy Pogoda z metodą contructor
+class Pogoda {
+  constructor() {
+    this.apiKey = '99dfe35fcb7de1ee';
+    this.city = city;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
