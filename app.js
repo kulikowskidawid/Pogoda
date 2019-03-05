6afadfb136c9d74e77c2b6e1dd7b4e261f757dee
kulikowskidawid <kulikowskidawid@outlook.com>
2019-03-05 21:48:54 +0100

    Ustawienia Zmiana Lokalizacji w aplikacji, wyświetlanie i funkcja pobrania pogody

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ const weatherLocation = storage.getLocationData();
 const pogoda = new Pogoda(weatherLocation.city);
 // Inicjalizacja obiektu UI
 const ui = new UI();
+
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// Event Zmiany Lokalizacji
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+
+// Zmiana Lokalizacji
+weather.changeLocation(city);
+
+//Ustaw Lokalizacje w Local Storage
+storage.setLocationData(city);
+
+//pobierz i wyświetl Pogodę
+  getWeather();
+
+// Zamknij  modal
+  $('#locModal').modal('hide');
+});
+
+//funkcja pobrania pogody
+function getWeather(){
+  weather.getWeather()
+    .then(results => {
+      ui.paint(results);
+    })
+    .catch(err => console.log(err));
+}
