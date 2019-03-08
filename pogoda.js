dc0a206cebb807fa7d1578be9b4a14ce2ddbfb77
kulikowskidawid <kulikowskidawid@outlook.com>
2019-03-08 16:36:34 +0100

    zmiana api z wunderground na apixa i poprawki w kodzie

--- pogoda.js
+++ pogoda.js
@@ -1,24 +1,25 @@
 // Uzyskany Klucz API z Wunderground cc557fbc884831f9
 
 // Deklaracja Klasy Pogoda z metodą contructor
-class Pogoda {
-  constructor() {
-    this.apiKey = '99dfe35fcb7de1ee';
-    this.city = city;
+class Weather {
+  constructor(name) {
+    this.apiKey = '814e389bec984aab982111118190703';
+    this.name = name;
   }
 
   //Fetch - Dynamiczne Pobranie zasobów z API
   async getWeather() {
-    const response = await fetch(`http://api.wunderground.com/api/cc557fbc884831f9/conditions/q/${this.city}.json`);
+    const response = await fetch(`http://api.apixu.com/v1/current.json?key=${this.apiKey}&q=${this.name}`);
+
     const responseData = await response.json();
 
     //Ustawienie aby funkcja return zwróciła informacje zawarte w "current_observation"
-    return responseData.current_observation
+    return responseData;
   } 
 
   // Zmiana Miejscowości 
-changeLocation(city) {
-  this.city = city;
+changeLocation(name) {
+  this.name = name;
 }
 }
 
