849f6b4e9901f99836f556f0557558bd3e0090a7
kulikowskidawid <kulikowskidawid@outlook.com>
2019-03-08 17:28:24 +0100

    Utworzenie Klasy UI z konstruktorem oraz Metody paint

--- ui.js
+++ ui.js
@@ -1,15 +1,26 @@
-//Deklaracja klasy UI z konstruktorem
-
+//utworzenie klasy z konstruktorem i elementami ktore będą wyświetlane 
 class UI {
   constructor() {
-    this.location = document.getElementById('location');
-    this.desc = document.getElementById('desc');
-    this.string = document.getElementById('temp');
-    this.details = document.getElementById('details');
-    this.icon = document.getElementById('icon');
-    this.humidity = document.getElementById('api1');
-    this.feelsLike = document.getElementById('api2');
-    this.dewpoint= document.getElementById('api3');
-    this.wind = document.getElementById('api4');
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc');
+    this.string = document.getElementById('w-string');
+    this.details = document.getElementById('w-details');
+    this.icon = document.getElementById('w-icon');
+    this.humidity = document.getElementById('w-humidity');
+    this.feelsLike = document.getElementById('w-feels-like');
+    this.cloud= document.getElementById('w-cloud');
+    this.wind = document.getElementById('w-wind');
   }
+  // Metoda paint która wypełni aplikacje rezultatami z API
+  paint(weather) {
+    this.location.textContent = weather.location.name;
+    this.string.textContent = `${weather.current.temp_c} °C`;
+    this.icon.setAttribute('src', `http:${weather.current.condition.icon}`);
+    this.humidity.textContent = `Wilgotność Powietrza: ${weather.current.humidity}%`;
+    this.feelsLike.textContent = `Odczuwalna Temperatura: ${weather.current.feelslike_c} °C`;
+    this.cloud.textContent = `Zachmurzenie: ${weather.current.cloud}%`;
+    this.wind.textContent = `Wiatr: ${weather.current.wind_kph} km/h`;
+  }
+}
+
 
